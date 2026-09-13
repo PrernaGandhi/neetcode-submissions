4856425020f4b41e8ff74cbe99e95409271bf743
Prerna Gandhi <prernagandhi97@gmail.com>
2026-09-13 12:05:11 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        // calculate ascii value and put in map if not present
+        // if present, add to the existing key
+        // map of int, and list of string
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s: strs) {
+            String key = ascii(s);
+            if(map.containsKey(key)) {
+                map.get(key).add(s);
+            } else {
+                List<String> val = new ArrayList();
+                val.add(s);
+                map.put(key, val);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public String ascii(String s) {
+        int freq[] = new int[26];
+        for(char ch : s.toCharArray()) {
+            freq[ch - 'a'] += 1;
+        }
+        return Arrays.toString(freq);
+    }
+}
