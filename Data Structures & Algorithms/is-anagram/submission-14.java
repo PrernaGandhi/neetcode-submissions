a2df65440282e76c2bdf0f0cd2e976954bb811d8
Prerna Gandhi <prernagandhi97@gmail.com>
2026-09-13 11:20:27 +0530

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // sort and then check if strings are equal
+        // N log N time complexity, space to sort
+        
+        return Arrays.equals(s.chars().sorted().toArray(), t.chars().sorted().toArray());
+    }
+
+    // public boolean isAnagram(String s, String t) {
+    //     // freq array of O(26), add freq of first string, then subtract and then check if the freq is 0 then anagram otherwise no
+
+    // }
+}
